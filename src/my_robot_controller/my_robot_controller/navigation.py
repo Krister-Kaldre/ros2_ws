5832e6eb9f2ab6577a82b0bd30cacb64cca3486d
Krister-Kaldre <kaldrekrister@gmail.com>
2024-03-19 17:43:16 +0200

    Working repo

---
+++ src/my_robot_controller/my_robot_controller/navigation.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from turtlesim.msg import Pose
+from sensor_msgs.msg import LaserScan
+import tf_transformations
+import time
+
+
+class TurtleNavigationNode(Node):
+
+    def __init__(self):
+        super().__init__("navigation")
+        self.get_logger().info("our navigation is started")
+
+        self.initial_pose_publisher = self.create_publisher(
+            PoseWithCovarianceStamped, "/initialpose",10)
+        
+        
+        self.goal_pose_publisher = self.create_publisher(
+            PoseStamped, "/goal_pose",10)
+        
+
+        self.odom_listener = self.create_subscription(
+            Odometry, "/odom_pose",self.odom_callback,10)
+        
+        #######################[initial location]#######################
+        initial_pose = PoseWithCovarianceStamped()
+        initial_pose.header.frame_id = 'map'
+        initial_pose.pose.pose.position.x = 0.0
+        initial_pose.pose.pose.position.y = 0.0
+        initial_pose.pose.pose.position.z = 0.0
+
+        
+        qq = tf_transformations.quaternion_from_euler(0,0,0)#x,y,z or roll, pitch,yawn
+        initial_pose.pose.pose.orientation.x = qq[0]
+        initial_pose.pose.pose.orientation.y = qq[1]
+        initial_pose.pose.pose.orientation.z = qq[2]
+        initial_pose.pose.pose.orientation.w = qq[3]
+        self.initial_pose_publisher.publish(initial_pose)
+        ##############################################
+        time.sleep(2)
+        #######################[destination]#######################
+        goal = PoseStamped()
+        goal.header.frame_id = 'map'
+        goal.pose.position.x = 3.5
+        goal.pose.position.y = 0.0
+        qq = tf_transformations.quaternion_from_euler(0,0,1.57)#x,y,z or roll, pitch,yawn
+        goal.pose.orientation.x = qq[0]
+        goal.pose.orientation.y = qq[1]
+        goal.pose.orientation.z = qq[2]
+        goal.pose.orientation.w = qq[3]
+        self.goal_pose_publisher.publish(goal)
+    def odom_callback(self,msg:Odometry):
+        pass
+    #def robot_controller(self,scan:LaserScan):
+    
+        # cmd = Twist()
+        # a = 2
+
+        # self._front = min(scan.ranges[:a+1] + scan.ranges[-a:])
+        # self._left = min(scan.ranges[89-a:89+a+1])
+        # self._back = min(scan.ranges[179-a:179+a+1])
+        # self._rigth = min(scan.ranges[269-a:269+a+1])
+        
+        
+        # if self._front < 1.0:
+        
+        #     if self._left < self._rigth:
+        #         cmd.linear.x = 0.2    
+        #         cmd.angular.z = -0.8
+        #     else:
+        #         cmd.linear.x = 0.2    
+        #         cmd.angular.z = 0.8
+        # else:
+        #     cmd.linear.x = 0.5    
+        #     cmd.angular.z = 0.0
+        
+            
+        #self._pose_publisher.publish(cmd)
+
+    
+
+
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = TurtleNavigationNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+if __name__ == '__main__':
+    main()
+
